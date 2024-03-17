c4564dcc78e69a9242910c66074b57114ed9bb4d
Fiona Mellink <fimellink@gmail.com>
2024-03-17 12:51:56 +1100

    Complete lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map ((movie)=> {
+        return <div key={movie.title}>
+          <h3>Title: {movie.title}</h3>
+          <h4>Time: {movie.time}</h4>
+          <ul>
+            {movie.genres.map ((genre)=> {
+              return <li key={genre}>{genre}</li>
+            })}
+          </ul>
+        </div>
+      })}
+    </div>
+  )
 }
 
 export default Movies;
